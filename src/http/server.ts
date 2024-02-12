0078882a44cce03694ac65237583652df2d86df5
Igor Santos <igorsantos9513@outlook.com>
2024-02-12 14:40:59 -0300

    feat: create poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,31 +1,14 @@
-import { Prisma, PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 import z from "zod";
+import { createPoll } from "./routes/create-poll";
 
 const app = fastify();
-const prisma = new PrismaClient();
-
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running at port: 3333");
 });
 
+app.register(createPoll);
+
 app.get("/hello", () => {
   return `hello NLW`;
-});
-
-//                              reply =? same as resonse, i guess.
-app.post("/polls", async (req, reply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(req.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({ poolId: poll.id });
 });
